1342dfef9ea3a5f1146a7cfde670f23a8be7bd3d
Christian Rauch <Rauch.Christian@gmx.de>
2025-12-19 01:16:41 +0100

    orientation+thrust

--- uav_controllers/src/controller_linear_acceleration_pid.cpp
+++ uav_controllers/src/controller_linear_acceleration_pid.cpp
@@ -120,21 +120,21 @@ public:
   controller_interface::return_type update_and_write_commands(
     const rclcpp::Time & /*time*/, const rclcpp::Duration & period) override
   {
-    // reference: linear acceleration
+    // reference: linear acceleration (world frame)
     const Eigen::Vector3d acc_ref{
       reference_interfaces_[0],
       reference_interfaces_[1],
       reference_interfaces_[2],
     };
 
-    // state: linear acceleration
+    // state: linear acceleration (body frame)
     const Eigen::Vector3d acc_state{
       state_interfaces_[0].get_optional().value_or(nan),
       state_interfaces_[1].get_optional().value_or(nan),
       state_interfaces_[2].get_optional().value_or(nan),
     };
 
-    // state: orientation
+    // state: orientation (world to body frame)
     const Eigen::Quaterniond q_state{
       state_interfaces_[6].get_optional().value_or(nan),  // w
       state_interfaces_[3].get_optional().value_or(nan),  // x
@@ -142,13 +142,75 @@ public:
       state_interfaces_[5].get_optional().value_or(nan),  // z
     };
 
-    // DBG
+    // rotate reference and state linear acceleration to the body frame
+    const Eigen::Vector3d acc_ref_body = q_state.conjugate() * acc_ref;
+
+    // linear acceleration error in body frame
+    const Eigen::Vector3d acc_err_body = acc_ref_body - acc_state;
+
+    // compute commands for x, y, z linear acceleration
+    // std::array<double, cmd_order.size()> cmds;
+    Eigen::Vector3d cmds;
+    for (size_t i = 0; i < 3; ++i)
+    {
+      cmds[i] = pid_controllers[i]->compute_command(acc_err_body[i], period);
+    }
+
+    // RCLCPP_INFO_STREAM(
+    //   get_node()->get_logger(),
+    //   std::fixed << std::setprecision(2) << "cmds (x,y,z): " << cmds.transpose());
+
+    // The thrust command is the magnitude of the commanded linear acceleration in the body frame.
+    // This is the output of the PID for the linear acceleration error.
+    // The thrust is applied along the body's z-axis.
+    const double thrust_z = pid_controllers[3]->compute_command(cmds.norm(), period);
+
+    // The desired orientation is the rotation of the z-axis of the body frame
+    // to align with the commanded linear acceleration vector.
+    // The commanded linear acceleration is `cmds` in the body frame.
+    // We want the body's z-axis to point in the direction of `cmds`.
+    // The current body z-axis in the world frame is q_state * [0,0,1].
+    // The desired body z-axis in the world frame is the normalized `cmds` vector.
+    // However, `cmds` here are the output of the PID for linear acceleration error in the body
+    // frame. This means `cmds` are the desired linear accelerations in the body frame. The
+    // orientation controller expects a quaternion reference. The thrust command is also derived
+    // from the linear acceleration.
+
+    // Compute the quaternion that rotates the z-axis of the body frame to the `cmds` vector.
+    const Eigen::Quaterniond q_cmd =
+      Eigen::Quaterniond().setFromTwoVectors(Eigen::Vector3d::UnitZ(), cmds.normalized());
+
+    // RCLCPP_INFO_STREAM(
+    //   get_node()->get_logger(), std::fixed << std::setprecision(2)
+    //                                        << "q_cmd (x,y,z,w): " << q_cmd.coeffs().transpose()
+    //                                        << " thrust_z: " << thrust_z);
+
+    bool status_ok = true;
+
+    // Set orientation commands
+    status_ok &= command_interfaces_[0].set_value(q_cmd.x());
+    status_ok &= command_interfaces_[1].set_value(q_cmd.y());
+    status_ok &= command_interfaces_[2].set_value(q_cmd.z());
+    status_ok &= command_interfaces_[3].set_value(q_cmd.w());
+
+    // Set thrust command
+    status_ok &= command_interfaces_[4].set_value(thrust_z);
+
+    if (status_ok)
+    {
+      return controller_interface::return_type::OK;
+    }
+    else
+    {
+      return controller_interface::return_type::ERROR;
+    }
     return controller_interface::return_type::OK;
   }
 
 private:
   static constexpr double nan = std::numeric_limits<double>::signaling_NaN();
-  static constexpr std::array<std::string, 4> cmd_order = {"x", "y", "z", "thrust"};
+  // The first three commands are for orientation.x, orientation.y, orientation.z
+  static constexpr std::array<std::string, 4> cmd_order = {"x", "y", "z"};
 
   std::string mixer_name;
   std::string orientation_controller_name;
