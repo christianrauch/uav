d652599ee39708e611bd8dea078debbb8f8e2fe9
Christian Rauch <Rauch.Christian@gmx.de>
2025-12-21 13:24:59 +0100

    bla

--- uav_controllers/src/controller_linear_acceleration_pid.cpp
+++ uav_controllers/src/controller_linear_acceleration_pid.cpp
@@ -122,40 +122,134 @@ public:
     const rclcpp::Time & /*time*/, const rclcpp::Duration & period) override
   {
     // reference: linear acceleration (world frame)
-    const Eigen::Vector3d acc_ref{
+    const Eigen::Vector3d acc_ref_world{
       reference_interfaces_[0],
       reference_interfaces_[1],
       reference_interfaces_[2],
     };
 
     // state: linear acceleration (body frame)
-    const Eigen::Vector3d acc_state{
+    const Eigen::Vector3d acc_state_body{
       state_interfaces_[0].get_optional().value_or(nan),
       state_interfaces_[1].get_optional().value_or(nan),
       state_interfaces_[2].get_optional().value_or(nan),
     };
 
     // state: orientation (world to body frame)
-    const Eigen::Quaterniond q_state{
+    const Eigen::Quaterniond q_state_body{
       state_interfaces_[6].get_optional().value_or(nan),  // w
       state_interfaces_[3].get_optional().value_or(nan),  // x
       state_interfaces_[4].get_optional().value_or(nan),  // y
       state_interfaces_[5].get_optional().value_or(nan),  // z
     };
 
-    // rotate reference and state linear acceleration to the body frame
-    const Eigen::Vector3d acc_ref_body = q_state.conjugate() * acc_ref;
+    // rotate reference linear acceleration to the body frame
+    const Eigen::Vector3d acc_ref_body = q_state_body.conjugate() * acc_ref_world;
+    // rotate state linear acceleration to the world frame
+    const Eigen::Vector3d acc_state_world = q_state_body * acc_state_body;
+
+    if (!g.allFinite())
+    {
+      g = acc_state_body.norm() * Eigen::Vector3d::UnitZ();
+    }
+
+    // overcome gravity
+    // const Eigen::Vector3d acc_ref_world_g = g + acc_ref_world;
+
+    RCLCPP_INFO_STREAM(get_node()->get_logger(), "###############################################");
+
+    RCLCPP_INFO_STREAM(
+      get_node()->get_logger(),
+      std::fixed << std::setprecision(2) << "acc state (body): " << acc_state_body.transpose());
+
+    RCLCPP_INFO_STREAM(
+      get_node()->get_logger(), std::fixed << std::setprecision(2) << "q state (x,y,z,w): "
+                                           << q_state_body.coeffs().transpose());
+
+    RCLCPP_INFO_STREAM(
+      get_node()->get_logger(),
+      std::fixed << std::setprecision(2) << "> acc state (world): " << acc_state_world.transpose());
+
+    RCLCPP_INFO_STREAM(
+      get_node()->get_logger(),
+      std::fixed << std::setprecision(2) << "acc ref (world): " << acc_ref_world.transpose());
+
+    RCLCPP_INFO_STREAM(
+      get_node()->get_logger(),
+      std::fixed << std::setprecision(2) << "> acc ref (body): " << acc_ref_body.transpose());
+
+    // STATES END
+
+    // the following assumes that we can only apply thrust along the body z-axis
+
+    // thrust * z-axis == acc_ref_world
+    // thrust * z-axis == (acc_ref_world - acc_state_world)
+
+    // the bodies z-axis in world frame
+    const Eigen::Vector3d unit_z_body = q_state_body.conjugate() * Eigen::Vector3d::UnitZ();
+
+    RCLCPP_INFO_STREAM(
+      get_node()->get_logger(),
+      std::fixed << std::setprecision(2) << "> z (body): " << unit_z_body.transpose());
+
+    // rotate such that body z-axis aligns with direction of acc_ref in world frame
+    const Eigen::Quaterniond q_cmd =
+      q_state_body.conjugate() *
+      Eigen::Quaterniond::FromTwoVectors(unit_z_body, acc_ref_world).normalized();
+
+    RCLCPP_INFO_STREAM(get_node()->get_logger(), "cmd: " << std::endl << q_cmd.toRotationMatrix());
+
+    // TODO: should we enforce a max orientation (30deg) w.r.t. to the world z?
 
     // linear acceleration error in body frame
-    const Eigen::Vector3d acc_err_body = acc_ref_body - acc_state;
+    // const Eigen::Vector3d acc_err_body = acc_ref_body - acc_state_body;
+
+    // linear acceleration error in body frame
+    const Eigen::Vector3d acc_err_world = acc_ref_world - acc_state_world;
+    // const Eigen::Vector3d acc_err_world = acc_ref_world - (acc_state_world + g);
+
+    RCLCPP_INFO_STREAM(
+      get_node()->get_logger(),
+      std::fixed << std::setprecision(2) << "> acc err (body): " << acc_err_world.transpose());
+
+    // if the error points in the opposite direction of the reference, invert the command magnitude
+    // const bool oppos_dir = std::signbit(acc_err_world.dot(acc_ref_world));
+
+    const double dot = acc_err_world.dot(unit_z_body);
+    // const double dot = (-acc_err_world).dot(unit_z_body);
+
+    RCLCPP_INFO_STREAM(get_node()->get_logger(), "dot: " << dot);
+
+    // const bool oppos_dir = (dot <= 0);
+
+    const double dir = dot / std::abs(dot);
+
+    // const Eigen::Quaterniond q_cmd =
+    //   Eigen::Quaterniond::FromTwoVectors(unit_z_body, acc_err_world.normalized()).normalized();
+
+    // const double thrust_z = acc_err_world.norm();
+    const double thrust_z = dir * pid_controllers[3]->compute_command(acc_err_world.norm(), period);
+    // const double thrust_z = pid_controllers[3]->compute_command(acc_err_world.norm(), period);
+
+    RCLCPP_INFO_STREAM(
+      get_node()->get_logger(),
+      std::fixed << std::setprecision(5) << "q cmd (x,y,z,w): " << q_cmd.coeffs().transpose());
+
+    RCLCPP_INFO_STREAM(
+      get_node()->get_logger(), std::fixed << std::setprecision(2) << "thrust: " << thrust_z << " ("
+                                           << acc_err_world.transpose() << ")");
+
+    // this is our target thrust error to minimise
+
+    // find orientation that minimises acc_err_body
 
     // compute commands for x, y, z linear acceleration
     // std::array<double, cmd_order.size()> cmds;
-    Eigen::Vector3d cmds;
-    for (size_t i = 0; i < 3; ++i)
-    {
-      cmds[i] = pid_controllers[i]->compute_command(acc_err_body[i], period);
-    }
+    // Eigen::Vector3d cmds;
+    // for (size_t i = 0; i < cmd_order.size(); ++i)
+    // {
+    //   cmds[i] = pid_controllers[i]->compute_command(acc_err_body[i], period);
+    // }
 
     // TODO: setting the target orientation should work without PIDs
 
@@ -166,7 +260,7 @@ public:
     // The thrust command is the magnitude of the commanded linear acceleration in the body frame.
     // This is the output of the PID for the linear acceleration error.
     // The thrust is applied along the body's z-axis.
-    const double thrust_z = pid_controllers[3]->compute_command(cmds.norm(), period);
+    // const double thrust_z = pid_controllers[3]->compute_command(acc_err_world.norm(), period);
 
     // The desired orientation is the rotation of the z-axis of the body frame
     // to align with the commanded linear acceleration vector.
@@ -180,13 +274,17 @@ public:
     // from the linear acceleration.
 
     // Compute the quaternion that rotates the z-axis of the body frame to the `cmds` vector.
-    const Eigen::Quaterniond q_cmd =
-      Eigen::Quaterniond().setFromTwoVectors(Eigen::Vector3d::UnitZ(), cmds.normalized());
+    // const Eigen::Quaterniond q_cmd =
+    //   Eigen::Quaterniond().setFromTwoVectors(Eigen::Vector3d::UnitZ(), cmds.normalized());
 
     // RCLCPP_INFO_STREAM(
     //   get_node()->get_logger(), std::fixed << std::setprecision(2)
     //                                        << "q_cmd (x,y,z,w): " << q_cmd.coeffs().transpose()
     //                                        << " thrust_z: " << thrust_z);
+
+    constexpr double eps = 1e-6;
+
+    assert(std::abs(q_cmd.norm() - 1) <= eps);
 
     bool status_ok = true;
 
@@ -198,6 +296,9 @@ public:
 
     // Set thrust command
     status_ok &= command_interfaces_[4].set_value(thrust_z);
+
+    // DBG
+    // status_ok &= command_interfaces_[4].set_value(1.0);
 
     if (status_ok)
     {
@@ -219,6 +320,8 @@ private:
   std::string orientation_controller_name;
   std::string sensor_name;
   std::array<std::shared_ptr<control_toolbox::Pid>, cmd_order.size()> pid_controllers;
+
+  Eigen::Vector3d g = Eigen::Vector3d::Zero() * nan;
 
   rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr sub_reference;
   realtime_tools::RealtimeThreadSafeBox<geometry_msgs::msg::Vector3> msg_reference;
