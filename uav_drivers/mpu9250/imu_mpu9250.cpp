7a604a2b9ff2cfd160d1acf82f8dea9e58535065
Christian Rauch <Rauch.Christian@gmx.de>
2026-01-10 14:50:32 +0100

    MPU9250 implementation via LibDriver project

---
+++ uav_drivers/mpu9250/imu_mpu9250.cpp
@@ -0,0 +1,53 @@
+#include <pluginlib/class_list_macros.hpp>
+#include <uav_drivers/imu_base.hpp>
+
+#include <driver_mpu9250_basic.h>
+
+namespace uav::drivers
+{
+
+namespace imu
+{
+
+class MPU9250 : public IMUInterfaceBase
+{
+private:
+  bool init_driver() override final
+  {
+    if (mpu9250_basic_init(MPU9250_INTERFACE_IIC, MPU9250_ADDRESS_AD0_LOW) != 0)
+    {
+      (void)mpu9250_basic_deinit();
+      return false;
+    }
+
+    return true;
+  }
+
+  bool read_imu_data() override final
+  {
+    imu.magnetic_field = {0, 0, 0};
+    if (
+      mpu9250_basic_read(
+        imu.linear_acceleration.data(), imu.angular_velocity.data(), imu.magnetic_field->data()) !=
+      0)
+    {
+      RCLCPP_ERROR_STREAM(get_logger(), "mpu9250_basic_read failed");
+      return false;
+    }
+
+    imu.temperature = 0;
+    if (mpu9250_basic_read_temperature(&(*imu.temperature)) != 0)
+    {
+      RCLCPP_ERROR_STREAM(get_logger(), "mpu9250_basic_read_temperature failed");
+      return false;
+    }
+
+    return true;
+  }
+};
+
+}  // namespace imu
+
+}  // namespace uav::drivers
+
+PLUGINLIB_EXPORT_CLASS(uav::drivers::imu::MPU9250, hardware_interface::SensorInterface)
