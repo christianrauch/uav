45366b1c73f6dbc0bc856058e8b3b99ac2d173df
Christian Rauch <Rauch.Christian@gmx.de>
2026-01-13 00:51:04 +0100

    [DBG] remove controllers

--- uav_description/launch/quad.launch.py
+++ uav_description/launch/quad.launch.py
@@ -67,11 +67,6 @@ def generate_launch_description():
             'fusion',
             'fusion_sensor_broadcaster',
             'magnetometer_broadcaster',
-            'control_allocation',
-            'angular_velocity',
-            # 'angular_position',
-            'attitude',
-            'attitude_mode',
         ],
     )
 
