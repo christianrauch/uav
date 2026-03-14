c4dfc20c0fc03136028ea86bf558003b12c970ad
Christian Rauch <Rauch.Christian@gmx.de>
2026-03-14 12:06:02 +0100

    publish RC

--- uav_controllers/src/rc_broadcaster.cpp
+++ uav_controllers/src/rc_broadcaster.cpp
@@ -1,15 +1,14 @@
 // base on the "imu_sensor_broadcaster" package (Apache License, Version 2.0) by Victor Lopez
 // https://github.com/ros-controls/ros2_controllers/tree/4.34.0/imu_sensor_broadcaster
 
+#include <cmath>
 #include <controller_interface/controller_interface.hpp>
+#include <mavros_msgs/msg/rc_out.hpp>
 #include <memory>
 #include <pluginlib/class_list_macros.hpp>
 #include <rclcpp_lifecycle/state.hpp>
 #include <realtime_tools/realtime_publisher.hpp>
-#include <semantic_components/magnetic_field_sensor.hpp>
-#include <sensor_msgs/msg/magnetic_field.hpp>
 #include <string>
-#include <vector>
 
 namespace rc_broadcaster
 {
@@ -35,36 +34,21 @@ public:
     const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
 protected:
-  std::unique_ptr<semantic_components::MagneticFieldSensor> magnetic_field_sensor_;
-
-  using StatePublisher = realtime_tools::RealtimePublisher<sensor_msgs::msg::MagneticField>;
-  rclcpp::Publisher<sensor_msgs::msg::MagneticField>::SharedPtr sensor_state_publisher_;
+  using StatePublisher = realtime_tools::RealtimePublisher<mavros_msgs::msg::RCOut>;
+  rclcpp::Publisher<mavros_msgs::msg::RCOut>::SharedPtr sensor_state_publisher_;
   std::unique_ptr<StatePublisher> realtime_publisher_;
-  sensor_msgs::msg::MagneticField state_message_;
+  mavros_msgs::msg::RCOut state_message_;
 };
 
-controller_interface::CallbackReturn RCBroadcaster::on_init()
-{
-  return CallbackReturn::SUCCESS;
-}
+controller_interface::CallbackReturn RCBroadcaster::on_init() { return CallbackReturn::SUCCESS; }
 
 controller_interface::CallbackReturn RCBroadcaster::on_configure(
   const rclcpp_lifecycle::State & /*previous_state*/)
 {
-  const std::string sensor_name = get_node()->declare_parameter<std::string>("sensor_name");
-  if (sensor_name.empty())
-  {
-    RCLCPP_FATAL_STREAM(
-      get_node()->get_logger(),
-      "Parameter 'sensor_name' is empty. Please specify a sensor name for the magnetometer.");
-    return controller_interface::CallbackReturn::ERROR;
-  }
-
-  magnetic_field_sensor_ = std::make_unique<semantic_components::MagneticFieldSensor>(sensor_name);
   try
   {
-    sensor_state_publisher_ = get_node()->create_publisher<sensor_msgs::msg::MagneticField>(
-      "~/magnetic_field", rclcpp::SystemDefaultsQoS());
+    sensor_state_publisher_ = get_node()->create_publisher<mavros_msgs::msg::RCOut>(
+      "~/rc_out", rclcpp::SystemDefaultsQoS());
     realtime_publisher_ = std::make_unique<StatePublisher>(sensor_state_publisher_);
   }
   catch (const std::exception & e)
@@ -75,49 +59,60 @@ controller_interface::CallbackReturn RCBroadcaster::on_configure(
     return CallbackReturn::ERROR;
   }
 
-  const std::string frame_id =
+  state_message_.header.frame_id =
     get_node()->declare_parameter<std::string>("frame_id", std::string{});
-  state_message_.header.frame_id = (frame_id.empty()) ? sensor_name : frame_id;
+  state_message_.channels.resize(8);
 
   RCLCPP_DEBUG(get_node()->get_logger(), "configure successful");
   return CallbackReturn::SUCCESS;
 }
 
-controller_interface::InterfaceConfiguration
-RCBroadcaster::command_interface_configuration() const
+controller_interface::InterfaceConfiguration RCBroadcaster::command_interface_configuration() const
 {
   controller_interface::InterfaceConfiguration command_interfaces_config;
   command_interfaces_config.type = controller_interface::interface_configuration_type::NONE;
   return command_interfaces_config;
 }
 
-controller_interface::InterfaceConfiguration
-RCBroadcaster::state_interface_configuration() const
+controller_interface::InterfaceConfiguration RCBroadcaster::state_interface_configuration() const
 {
   controller_interface::InterfaceConfiguration state_interfaces_config;
   state_interfaces_config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
-  state_interfaces_config.names = magnetic_field_sensor_->get_state_interface_names();
+  for (int i = 1; i <= 8; ++i)
+  {
+    state_interfaces_config.names.push_back("rc/channel/" + std::to_string(i));
+  }
   return state_interfaces_config;
 }
 
 controller_interface::CallbackReturn RCBroadcaster::on_activate(
   const rclcpp_lifecycle::State & /*previous_state*/)
 {
-  magnetic_field_sensor_->assign_loaned_state_interfaces(state_interfaces_);
   return CallbackReturn::SUCCESS;
 }
 
 controller_interface::CallbackReturn RCBroadcaster::on_deactivate(
   const rclcpp_lifecycle::State & /*previous_state*/)
 {
-  magnetic_field_sensor_->release_interfaces();
   return CallbackReturn::SUCCESS;
 }
 
 controller_interface::return_type RCBroadcaster::update(
   const rclcpp::Time & time, const rclcpp::Duration & /*period*/)
 {
-  magnetic_field_sensor_->get_values_as_message(state_message_);
+  for (const auto & interface : state_interfaces_)
+  {
+    for (size_t i = 1; i <= 8; ++i)
+    {
+      if (interface.get_name() == "rc/channel/" + std::to_string(i))
+      {
+        if (!std::isnan(interface.get_value()))
+        {
+          state_message_.channels[i - 1] = static_cast<uint16_t>(interface.get_value());
+        }
+      }
+    }
+  }
 
   if (realtime_publisher_)
   {
@@ -128,7 +123,6 @@ controller_interface::return_type RCBroadcaster::update(
   return controller_interface::return_type::OK;
 }
 
-PLUGINLIB_EXPORT_CLASS(
-  rc_broadcaster::RCBroadcaster, controller_interface::ControllerInterface)
+PLUGINLIB_EXPORT_CLASS(rc_broadcaster::RCBroadcaster, controller_interface::ControllerInterface)
 
-}  // namespace magnetometer_broadcaster
+}  // namespace rc_broadcaster
