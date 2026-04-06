cdcdf4876fda6fa2cdb0fe68044454442b0ee975
Christian Rauch <Rauch.Christian@gmx.de>
2026-04-06 21:38:13 +0200

    config

--- uav_description/launch/quad.launch.py
+++ uav_description/launch/quad.launch.py
@@ -70,6 +70,7 @@ def generate_launch_description():
             'rc_broadcaster',
             'control_allocation',
             'angular_velocity',
+            'angular_velocity_reference_broadcaster',
             'attitude',
             'attitude_mode',
         ],
@@ -86,7 +87,7 @@ def generate_launch_description():
             {'modes': ['none', 'attitude', '']},
             {'mode_controllers': {
                 'attitude': [
-                    'control_allocation', 'angular_velocity', 'attitude', 'attitude_mode',
+                    'control_allocation', 'angular_velocity', 'angular_velocity_reference_broadcaster', 'attitude', 'attitude_mode',
                 ],
             }},
         ],
