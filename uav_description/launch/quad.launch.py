08dd770bcd5bc7232ded0e04e501cda5926583fb
Christian Rauch <Rauch.Christian@gmx.de>
2026-08-11 00:52:35 +0200

    add battery_state_broadcaster with mock component

--- uav_description/launch/quad.launch.py
+++ uav_description/launch/quad.launch.py
@@ -69,6 +69,7 @@ def generate_launch_description():
             'fusion_sensor_broadcaster',
             'rotor_state_broadcaster',
             'rc_broadcaster',
+            'battery_state_broadcaster',
         ],
     )
 
